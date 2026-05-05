e39d394cf3a99e6a0e4834105f6730db152ad499
eliza <elizasouza1995a@gmail.com>
2026-05-05 15:39:40 +0100

    Fix Firebase storage bucket configuration

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDYGU13_z3fIyD73wo6imAIgKseN19gVd4",
+  authDomain: "reuse-app-aff90.firebaseapp.com",
+  projectId: "reuse-app-aff90",
+  storageBucket: "reuse-app-aff90.appspot.com",
+  messagingSenderId: "569785583931",
+  appId: "1:569785583931:web:55274669979766cac7cf1e",
 }
 
 const app = initializeApp(firebaseConfig)
